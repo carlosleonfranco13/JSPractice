cfe17abaa8bb0011c2a93dda31276d1cb598f712
carlos <carlosleonfranco13@gmail.com>
2023-05-04 14:31:14 -0500

    3 día de practicando bases 2

---
+++ multipleAsyncAwaitPerf.js
@@ -0,0 +1,46 @@
+// MÚLTIPLES ASYNC / AWAIT Y PERFORMANCE (MULTIPLES LLAMADOS)
+
+const url = "https://jsonplaceholder.typicode.com/comments";
+const url2 = "https://jsonplaceholder.typicode.com/photos";
+
+const consultarAPI = async () => {
+
+    const inicio = performance.now()
+
+    const respuesta = await fetch(url)
+    const resultado = await respuesta.json()
+    // console.log(resultado);
+
+    // console.log('Iniciando 2da consulta')
+
+    const respuesta2 = await fetch(url2)
+    const resultado2 = await respuesta2.json()
+    // console.log(resultado2);
+
+    const fin = performance.now()
+
+    console.log(`Ejecución PRIMER async: ${fin - inicio} ms`)
+  };
+  
+  consultarAPI();
+
+
+
+
+  const consultarAPI2 = async () => {
+
+    const inicio = performance.now()
+  
+    const [ respuesta, respuesta2] = await Promise.all( [fetch(url), fetch(url2)] ) 
+    const resultado = await respuesta.json()
+    const resultado2 = await respuesta2.json()
+
+    // console.log(resultado);
+    // console.log(resultado2);
+
+    const fin = performance.now()
+
+    console.log(`Ejecución SEGUNDO async: ${fin - inicio} ms`)
+  };
+  
+  consultarAPI2();
